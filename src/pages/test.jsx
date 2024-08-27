52e6f475a349575d10f34023d5ebda6fda552c0a
rin6-515 <tai.rinshiyou515@gmail.com>
2024-08-27 18:48:20 +0900

    umiフレームワークについて簡単なお試し

---
+++ src/pages/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Button} from 'antd'
+
+const test = () => {
+  return (
+    <div>
+      hellow test page
+      <Button type='primary'> クリック</Button>
+    </div>
+  )
+}
+
+export default test
